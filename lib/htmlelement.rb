0cfd67db289d14c560509dd6f1848de840b66c67
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2011-08-08 00:08:27 +0900

    add htmlelement.rb

---
+++ lib/htmlelement.rb
@@ -0,0 +1,197 @@
+#!/usr/bin/env ruby
+
+require 'kconv'
+
+# if you set "$HtmlElementXhtmlMode = true" before "require 'htmlelement'"
+# resulting elements should be in xhtml format.
+
+class HtmlElement
+
+  module CHARSET
+    EUC_JP = "EUC-JP"
+    SJIS = "Shift_JIS"
+    UTF8 = "UTF-8"
+    LATIN1 = "ISO-8859-1"
+  end
+
+  Html4Doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+  "http://www.w3.org/TR/html4/loose.dtd">'.split(/\r?\n/o).join($/)
+
+  Xhtml1Doctype = '<?xml version="1.0" encoding="%s"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.split(/\r?\n/o).join($/)
+
+
+
+  ESC = {
+    '&' => '&amp;',
+    '"' => '&quot;',
+    '<' => '&lt;',
+    '>' => '&gt;'
+  }
+
+  DECODE = ESC.invert
+  CharEntityPat = /#{DECODE.keys.join("|")}/
+  
+  TagFormats = Hash.new("<%s%s>%s</%s>")
+  
+  [[%w(html body div table colgroup thead tbody ul ol dl head p pre blockquote),"<%s%s>#{$/}%s</%s>#{$/}"],
+   [%w(dt dd tr title h1 h2 h3 h4 h5 h6),"<%s%s>%s</%s>#{$/}"],
+   [%w(li col),"<%s%s>%s#{$/}"],
+   [%w(img meta link base input), "<%s%s>#{$/}"]
+  ].each do |tags,format|
+    tags.each do |tag|
+      TagFormats[tag] = format
+    end
+  end
+  
+  TagFormats[""] = "%s%s%s"
+
+  Html5Tags = %w(article section hgroup aside nav menu header footer menu figure details legend)
+  XhtmlTagFormats = TagFormats.dup
+  XhtmlTagFormats.each do |key, value|
+    if value == "<%s%s>%s#{$/}"
+#      XhtmlTagFormats.delete(key)
+      XhtmlTagFormats[key] = "<%s%s>%s</%s>#{$/}"
+    end
+    if value == "<%s%s>#{$/}"
+      XhtmlTagFormats[key] = "<%s%s />#{$/}"
+    end
+  end
+
+  %w(img).each do |tag|
+    XhtmlTagFormats[tag] =  "<%s%s/>#{$/}"
+  end
+
+  def HtmlElement.escape(str)
+    str.gsub(/[&"<>]/on) {|pat| ESC[pat] }
+  end
+
+  def HtmlElement.decode(str)
+    str.gsub(CharEntityPat) {|ent| DECODE[ent]}
+  end
+
+  def initialize(tagname)
+    @parent = nil
+    @tagname = tagname
+    @children = []
+    @attributes = {}
+    @end_comment_not_added = true
+  end
+
+  attr_reader :tagname
+  attr_accessor :parent, :children
+
+  def empty?
+    @children.empty?
+  end
+
+  def push(child)
+    @children.push child
+    child.parent = self if child.kind_of? HtmlElement
+    self
+  end
+
+  def pop
+    @children.pop
+  end
+
+  def []=(attribute, value)
+    @attributes[attribute] = value
+  end
+
+  def [](attribute)
+    @attributes[attribute]
+  end
+  
+  def format_attributes
+    @attributes.collect do |attr,value|
+      ' %s="%s"'%[attr,value.to_s]
+    end.sort.join("")
+  end
+  private :format_attributes
+
+  def add_end_comment_for_div
+    if @tagname == "div" and @end_comment_not_added
+      id_or_class = self["id"]||self["class"]
+      self.push HtmlElement.comment("end of #{id_or_class}") if id_or_class
+      @end_comment_not_added = false
+    end
+  end
+
+  if $HtmlElementXhtmlMode
+    def to_s
+      add_end_comment_for_div
+      XhtmlTagFormats[@tagname]%[@tagname, format_attributes, @children, @tagname]
+    end
+
+    def HtmlElement.doctype(encoding="euc-jp")
+      Xhtml1Doctype%[encoding]
+    end
+  else
+    def to_s
+      add_end_comment_for_div
+      TagFormats[@tagname]%[@tagname, format_attributes, @children, @tagname]
+    end
+
+    def HtmlElement.doctype(encoding="euc-jp")
+      Html4Doctype
+    end
+  end
+  alias to_str to_s
+
+  def HtmlElement.comment(content)
+    "<!-- #{content} -->#{$/}"
+  end
+      
+  def self.create(tagname,content=nil)
+    if Html5Tags.include? tagname
+      tag = self.new("div")
+      tag["class"] = tagname
+    else
+      tag = self.new(tagname)
+    end
+    tag.push content if not content.nil?
+    yield tag if block_given?
+    tag
+  end
+
+  def HtmlElement.urlencode(str)
+    str.toutf8.gsub(/[^\w\.\-]/n) {|ch| format('%%%02X', ch[0]) }
+  end
+
+  def HtmlElement.urldecode(str)
+    utf = str.gsub(/%\w\w/) {|ch| [ch[-2,2]].pack('H*') }
+    return utf.tosjis if $KCODE =~ /^s/io
+    return utf.toeuc if $KCODE =~ /^e/io
+    utf
+  end
+
+
+end
+  
+#def Tag(tagname,content=nil)
+#  tag = HtmlElement.new(tagname)
+#  tag.push content if not content.nil?
+#  yield tag if block_given?
+#  tag
+#end
+
+def Tag(tagname,content=nil)
+  HtmlElement.create(tagname,content)
+end
+
+class XhtmlElement < HtmlElement
+
+  def to_s
+    XhmlTagFormats[@tagname]%[@tagname, format_attributes, @children, @tagname]
+  end
+  alias to_str to_s
+
+  def XhtmlElement.create(tagname,content=nil)
+    tag = HtmlElement.new(tagname)
+    tag.push content if not content.nil?
+    yield tag if block_given?
+    tag
+  end
+end
