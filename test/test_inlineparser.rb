00f2d69d983501155c200c1785971d0f274b1212
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2011-08-09 05:42:40 +0900

    change tests so that they directly use PseudoHikiInlineParser::InlineStack

--- test/test_inlineparser.rb
+++ test/test_inlineparser.rb
@@ -5,9 +5,9 @@ require 'lib/pseudohiki/inlineparser'
 
 
 class TC_PsudoHikiInlineParser < Test::Unit::TestCase
-  def test_compile_token_pat
-    parser = PseudoHikiInlineParser.new
-    assert_equal(/'''|\{\{|''|\[\[|==|\}\}|\]\]|\|/,parser.stack.token_pat)
+  def test_inlinestack_compile_token_pat
+    parser = PseudoHikiInlineParser::InlineStack.new("")
+    assert_equal(/'''|\{\{|''|\[\[|==|\}\}|\]\]|\|/,parser.token_pat)
   end
 
   def test_inlinestack_split_into_tokens
@@ -21,31 +21,31 @@ class TC_PsudoHikiInlineParser < Test::Unit::TestCase
   end
 
   def test_inlinestack_parse
-    parser = PseudoHikiInlineParser.new("As {{''another test'' case}}, '''this part''' must be in <strong>.")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("As {{''another test'' case}}, '''this part''' must be in <strong>.")
+    tree = parser.parse.tree
     assert_equal([["As "], [[["another test"]], [" case"]], [", "], [["this part"]], [" must be in <strong>."]],tree)
-    parser = PseudoHikiInlineParser.new("this is a line that ends with a {{node}}")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("this is a line that ends with a {{node}}")
+    tree = parser.parse.tree
     assert_equal([["this is a line that ends with a "],[["node"]]],tree)
-    parser = PseudoHikiInlineParser.new("this is another line that ends with a {{node")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("this is another line that ends with a {{node")
+    tree = parser.parse.tree
     assert_equal([["this is another line that ends with a "],[["node"]]],tree)
   end
 
   def test_inlinestack_convert_last_node_into_leaf
-    parser = PseudoHikiInlineParser.new("this is another line that ends with a {{node")
-    stack = parser.stack.parse
+    parser = PseudoHikiInlineParser::InlineStack.new("this is another line that ends with a {{node")
+    stack = parser.parse
     assert_equal([["this is another line that ends with a "],[["node"]]],stack.tree)
     stack.convert_last_node_into_leaf
     assert_equal([["this is another line that ends with a "],["{{"], ["node"]],stack.tree)
-    parser = PseudoHikiInlineParser.new("As {{''another '''test'' case}}, '''this part''' must be in <strong>.")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("As {{''another '''test'' case}}, '''this part''' must be in <strong>.")
+    tree = parser.parse.tree
     assert_equal([["As "], [[["another "], ["'''"], ["test"]], [" case"]], [", "], [["this part"]], [" must be in <strong>."]],tree)
-    parser = PseudoHikiInlineParser.new("As {{''another ]]test case with a [[node]]''.}}, '''this part''' must be in <strong>.")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("As {{''another ]]test case with a [[node]]''.}}, '''this part''' must be in <strong>.")
+    tree = parser.parse.tree
     assert_equal([["As "], [[["another "], ["]]"], ["test case with a "], [["node"]]], ["."]], [", "], [["this part"]], [" must be in <strong>."]],tree)
-    parser = PseudoHikiInlineParser.new("As {{''another {{test'' case}}, '''this part''' must be in <strong>.")
-    tree = parser.stack.parse.tree
+    parser = PseudoHikiInlineParser::InlineStack.new("As {{''another {{test'' case}}, '''this part''' must be in <strong>.")
+    tree = parser.parse.tree
     assert_equal([["As "], [[["another "], ["{{"], ["test"]], [" case"]], [", "], [["this part"]], [" must be in <strong>."]],tree)
   end
 end
