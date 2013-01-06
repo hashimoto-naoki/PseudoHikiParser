b79031fb73a7f3f75c0d1eb9ac38a9881561c842
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2013-01-06 14:46:56 +0900

    add newlines to HtmlElement.doctype and HtmlElement.doctype

--- lib/htmlelement.rb
+++ lib/htmlelement.rb
@@ -19,11 +19,11 @@ class HtmlElement
   end
 
   Html4Doctype = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-  "http://www.w3.org/TR/html4/loose.dtd">'.split(/\r?\n/o).join($/)
+  "http://www.w3.org/TR/html4/loose.dtd">'.split(/\r?\n/o).join($/)+"#{$/}"
 
   Xhtml1Doctype = '<?xml version="1.0" encoding="%s"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.split(/\r?\n/o).join($/)
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'.split(/\r?\n/o).join($/)+"#{$/}"
 
 
 
