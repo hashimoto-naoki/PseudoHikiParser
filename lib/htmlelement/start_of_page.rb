f3628a628af294c98ae1fc7ec8beeb9fb70e802c
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2013-01-22 05:37:49 +0900

    added start_of_page.rb that adds a "Start of page" link at the end of each heading2 block

---
+++ lib/htmlelement/start_of_page.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+class HtmlElement
+
+  def self.create_start_of_page(label, dest)
+    self.create("div").configure do |to|
+      to["class"] = "to_top"
+      a = self.create("a", label)
+      a["href"] = dest
+      to.push a
+    end
+  end
+
+  def self.set_start_of_page(label = "Start of page", dest = "#start_of_page")
+    @@start_of_page = self.create_start_of_page(label, dest)
+  end
+
+  self.set_start_of_page
+
+  def add_end_comment_for_div
+    if @tagname == "div" and @end_comment_not_added
+      id_or_class = self["id"]||self["class"]
+      self.push @@start_of_page if id_or_class == "section h2"
+      self.push HtmlElement.comment("end of #{id_or_class}") if id_or_class
+      @end_comment_not_added = false
+    end
+  end
+end
