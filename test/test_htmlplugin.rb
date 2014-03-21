a0840265dd8e9804f934afa0a9dc22938ddb1d48
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-03-22 00:13:18 +0900

    htmlplugin.rb: you can escape inline tags now.

--- test/test_htmlplugin.rb
+++ test/test_htmlplugin.rb
@@ -11,4 +11,10 @@ class TC_HtmlPlugin < MiniTest::Unit::TestCase
     tree = InlineParser.new("{{co2}} represents the carbon dioxide.").parse.tree
     assert_equal("CO<sub>2</sub> represents the carbon dioxide.",tree.accept(formatter).to_s)
   end
+
+  def test_escape_inline_tags
+    formatter = HtmlFormat.get_plain
+    tree = InlineParser.new("a line with an inline tag such as {{''}}").parse.tree
+    assert_equal("a line with an inline tag such as ''",tree.accept(formatter).to_s)
+  end
 end
