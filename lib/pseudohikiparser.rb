ed22a387fc91c08d67eb6dc01906280a35d20d15
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-05-12 02:35:07 +0900

    pseudohikiparser.rb: the definition of class methods was in a wrong place.

--- lib/pseudohikiparser.rb
+++ lib/pseudohikiparser.rb
@@ -24,43 +24,43 @@ module PseudoHiki
       Formatter[preset_options] = formatter.create(options)
       PRESET_OPTIONS[type] = preset_options
       TYPE_TO_FORMATTER[type] = formatter
+    end
 
-      class << self
-        def format(hiki_data, format_type, options=nil, &block)
-          tree = BlockParser.parse(hiki_data)
+    class << self
+      def format(hiki_data, format_type, options=nil, &block)
+        tree = BlockParser.parse(hiki_data)
 
-          if options
-            Formatter[[format_type, options]] ||= TYPE_TO_FORMATTER[format_type].create(options)
-          else
-            Formatter[PRESET_OPTIONS[format_type]]
-          end.format(tree).tap do |formatted|
-            block.call(formatted) if block
-          end.to_s
-        end
+        if options
+          Formatter[[format_type, options]] ||= TYPE_TO_FORMATTER[format_type].create(options)
+        else
+          Formatter[PRESET_OPTIONS[format_type]]
+        end.format(tree).tap do |formatted|
+          block.call(formatted) if block
+        end.to_s
+      end
 
-        def to_html(hiki_data, &block)
-          format(hiki_data, :html, options=nil, &block)
-        end
+      def to_html(hiki_data, &block)
+        format(hiki_data, :html, options=nil, &block)
+      end
 
-        def to_xhtml(hiki_data, &block)
-          format(hiki_data, :xhtml, options=nil, &block)
-        end
+      def to_xhtml(hiki_data, &block)
+        format(hiki_data, :xhtml, options=nil, &block)
+      end
 
-        def to_html5(hiki_data, &block)
-          format(hiki_data, :html5, options=nil, &block)
-        end
+      def to_html5(hiki_data, &block)
+        format(hiki_data, :html5, options=nil, &block)
+      end
 
-        def to_plain(hiki_data, &block)
-          format(hiki_data, :plain, options=nil, &block)
-        end
+      def to_plain(hiki_data, &block)
+        format(hiki_data, :plain, options=nil, &block)
+      end
 
-        def to_markdown(hiki_data, &block)
-          format(hiki_data, :markdown, options=nil, &block)
-        end
+      def to_markdown(hiki_data, &block)
+        format(hiki_data, :markdown, options=nil, &block)
+      end
 
-        def to_gfm(hiki_data, &block)
-          format(hiki_data, :gfm, options=nil, &block)
-        end
+      def to_gfm(hiki_data, &block)
+        format(hiki_data, :gfm, options=nil, &block)
       end
     end
   end
