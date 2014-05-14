7e1e2e467f455d3a05bcd7b6ac2f5bd928e57db0
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-05-15 06:24:29 +0900

    moved again definitions in "if $0 == __FILE__" clause of converter.rb to bin/pseudohiki2html.rb

--- lib/pseudohiki/converter.rb
+++ lib/pseudohiki/converter.rb
@@ -409,21 +409,3 @@ USAGE: #{File.basename(__FILE__)} [options]") do |opt|
     end
   end
 end
-
-if $0 == __FILE__
-  options = PseudoHiki::OptionManager.new
-  options.set_options_from_command_line
-
-  if $KCODE
-    PseudoHiki::OptionManager::ENCODING_REGEXP.each do |pat, encoding|
-      options[:encoding] = encoding if pat =~ $KCODE and not options[:force]
-    end
-  end
-
-  PseudoHiki::OptionManager.remove_bom
-  input_lines = ARGF.readlines.map {|line| line.encode(options.charset) }
-  options.set_options_from_input_file(input_lines)
-  html = PseudoHiki::PageComposer.new(options).compose_html(input_lines)
-
-  options.open_output {|out| out.puts html }
-end
