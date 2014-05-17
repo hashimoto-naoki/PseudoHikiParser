e24300cd5da1f7d8fa2343372c3fa8a505e66615
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-05-17 11:53:59 +0900

    cosmetic changes: fixed the indentation of class method definitions

--- lib/pseudohikiparser.rb
+++ lib/pseudohikiparser.rb
@@ -32,67 +32,67 @@ module PseudoHiki
       @@type_to_formatter[type] = formatter
     end
 
-      # Converts <hiki_data> into a format specified by <format_type>
-      #
-      # <hiki_data> should be a string or an array of strings
-      #
-      # Options for <format_type> are:
-      # [:html] HTML4.1
-      # [:xhtml] XHTML1.0
-      # [:html5] HTML5
-      # [:plain] remove all of tags
-      # [:plain_verbose] similar to :plain, but certain information such as urls in link tags will be kept
-      # [:markdown] Markdown
-      # [:gfm] GitHub Flavored Markdown
-      #
-      def self.format(hiki_data, format_type, options=nil, &block)
-        tree = BlockParser.parse(hiki_data)
+    # Converts <hiki_data> into a format specified by <format_type>
+    #
+    # <hiki_data> should be a string or an array of strings
+    #
+    # Options for <format_type> are:
+    # [:html] HTML4.1
+    # [:xhtml] XHTML1.0
+    # [:html5] HTML5
+    # [:plain] remove all of tags
+    # [:plain_verbose] similar to :plain, but certain information such as urls in link tags will be kept
+    # [:markdown] Markdown
+    # [:gfm] GitHub Flavored Markdown
+    #
+    def self.format(hiki_data, format_type, options=nil, &block)
+      tree = BlockParser.parse(hiki_data)
 
-        if options
-          @@formatter[[format_type, options]] ||= @@type_to_formatter[format_type].create(options)
-        else
-          @@formatter[@@preset_options[format_type]]
-        end.format(tree).tap do |formatted|
-          block.call(formatted) if block
-        end.to_s
-      end
+      if options
+        @@formatter[[format_type, options]] ||= @@type_to_formatter[format_type].create(options)
+      else
+        @@formatter[@@preset_options[format_type]]
+      end.format(tree).tap do |formatted|
+        block.call(formatted) if block
+      end.to_s
+    end
 
-      # Converts <hiki_data> into HTML4.1
-      #
-      #
-      def self.to_html(hiki_data, &block)
-        format(hiki_data, :html, options=nil, &block)
-      end
+    # Converts <hiki_data> into HTML4.1
+    #
+    #
+    def self.to_html(hiki_data, &block)
+      format(hiki_data, :html, options=nil, &block)
+    end
 
-      # Converts <hiki_data> into XHTML1.0
-      #
-      def self.to_xhtml(hiki_data, &block)
-        format(hiki_data, :xhtml, options=nil, &block)
-      end
+    # Converts <hiki_data> into XHTML1.0
+    #
+    def self.to_xhtml(hiki_data, &block)
+      format(hiki_data, :xhtml, options=nil, &block)
+    end
 
-      # Converts <hiki_data> into HTML5
-      #
-      def self.to_html5(hiki_data, &block)
-        format(hiki_data, :html5, options=nil, &block)
-      end
+    # Converts <hiki_data> into HTML5
+    #
+    def self.to_html5(hiki_data, &block)
+      format(hiki_data, :html5, options=nil, &block)
+    end
 
-      # Converts <hiki_data> into plain texts without tags
-      #
-      def self.to_plain(hiki_data, &block)
-        format(hiki_data, :plain, options=nil, &block)
-      end
+    # Converts <hiki_data> into plain texts without tags
+    #
+    def self.to_plain(hiki_data, &block)
+      format(hiki_data, :plain, options=nil, &block)
+    end
 
-      # Converts <hiki_data> into Markdown
-      #
-      def self.to_markdown(hiki_data, &block)
-        format(hiki_data, :markdown, options=nil, &block)
-      end
+    # Converts <hiki_data> into Markdown
+    #
+    def self.to_markdown(hiki_data, &block)
+      format(hiki_data, :markdown, options=nil, &block)
+    end
 
-      # Converts <hiki_data> into GitHub Flavored Markdown
-      #
-      def self.to_gfm(hiki_data, &block)
-        format(hiki_data, :gfm, options=nil, &block)
-      end
+    # Converts <hiki_data> into GitHub Flavored Markdown
+    #
+    def self.to_gfm(hiki_data, &block)
+      format(hiki_data, :gfm, options=nil, &block)
+    end
   end
 end
 
