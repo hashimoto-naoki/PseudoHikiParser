586273b4c24c5d9bb79be698cbdb262d5303dd08
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-10-06 00:00:36 +0900

    added PseudoHiki::Utils::NodeCollector

---
+++ lib/pseudohiki/utils.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'pseudohiki/inlineparser'
+require 'pseudohiki/blockparser'
+
+module PseudoHiki
+  module Utils
+
+    class NodeCollector
+      attr_reader :nodes
+
+      def self.select(tree, &condition)
+        collector = new(&condition)
+        collector.visit(tree)
+        collector.nodes
+      end
+
+      def initialize (&condition)
+        @nodes = []
+        @condition = condition
+      end
+
+      def visit(tree)
+        if @condition.call(tree)
+          @nodes.push tree
+        else 
+          tree.each do |node|
+            node.accept(self) if node.respond_to? :accept
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/test_utils.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require 'pseudohikiparser'
+require 'pseudohiki/utils'
+
+class TC_NodeCollector < MiniTest::Unit::TestCase
+
+  def setup
+    @input_text = <<TEXT
+!Title
+
+!![heading1]Heading1
+
+paragaraph
+
+!!Heading2
+
+paragragh
+
+!![heading3]Heading3
+
+paragraph
+TEXT
+  end
+
+  def test_nodecollector
+    tree = PseudoHiki::BlockParser.parse(@input_text)
+    nodes = PseudoHiki::Utils::NodeCollector.select(tree) do |node|
+      node.kind_of?(PseudoHiki::BlockParser::HeadingLeaf) and
+        node.nominal_level == 2 and
+        node.node_id
+    end
+
+    selected_headings = [[["Heading1\n"]],
+                [["Heading3\n"]]]
+
+    assert_equal(selected_headings, nodes)
+  end
+end
+
