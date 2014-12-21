6a4dc061254126821a2f1e042038faae534f1b92
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2014-12-21 12:57:55 +0900

    refactoring of BlockNode#nominal_level(): removed the unnecessary return statement

--- lib/pseudohiki/blockparser.rb
+++ lib/pseudohiki/blockparser.rb
@@ -132,8 +132,7 @@ module PseudoHiki
       attr_accessor :node_id
 
       def nominal_level
-        return nil unless first
-        first.nominal_level
+        first.nominal_level if first
       end
 
       def push_self(stack)
