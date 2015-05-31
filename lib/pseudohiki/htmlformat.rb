75d4dbaa7f83c955761a5dc959db4d154df492d6
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-05-31 19:36:16 +0900

    removed an unnecessary space for a consistent coding sytle

--- lib/pseudohiki/htmlformat.rb
+++ lib/pseudohiki/htmlformat.rb
@@ -196,7 +196,7 @@ module PseudoHiki
         super(tree).tap do |element|
           heading_level = "h#{tree.first.nominal_level}"
           element[CLASS] ||= heading_level
-          element[CLASS] +=  SPACE + heading_level unless element[CLASS] == heading_level
+          element[CLASS] += SPACE + heading_level unless element[CLASS] == heading_level
         end
       end
     end
