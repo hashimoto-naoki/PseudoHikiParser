fe8b99d58381dff0973c84cb5f2d77998f9f0c29
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-07-06 07:37:59 +0900

    added BlockParser.auto_linker

--- test/test_autolink.rb
+++ test/test_autolink.rb
@@ -68,4 +68,21 @@ HTML
     tree = BlockParser.parse(text.lines.to_a, auto_linker)
     assert_equal(xhtml, XhtmlFormat.format(tree).to_s)
   end
+
+  def test_link_wiki_name_in_quote
+    text = <<TEXT
+""a line with a url http://www.example.org/ , an ^EscapedWikiName and a WikiName.
+TEXT
+
+    xhtml = <<HTML
+<blockquote>
+<p>
+a line with a url <a href="http://www.example.org/">http://www.example.org/</a> , an EscapedWikiName and a <a href="WikiName">WikiName</a>.
+</p>
+</blockquote>
+HTML
+    auto_linker = AutoLink::WikiName.new({:wiki_name => true, :escape_wiki_name => true})
+    tree = BlockParser.parse(text.lines.to_a, auto_linker)
+    assert_equal(xhtml, XhtmlFormat.format(tree).to_s)
+  end
 end
