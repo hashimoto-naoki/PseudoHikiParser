5bd113c9ac46bdd44fd3f1d346fe505c4ebffc1b
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-10-13 07:57:31 +0900

    refactoring of PageComposer#create_main()

--- lib/pseudohiki/converter.rb
+++ lib/pseudohiki/converter.rb
@@ -167,8 +167,8 @@ module PseudoHiki
     end
 
     def create_main(toc, body, h1)
-      return @plain_composer.create_main(toc, body, h1) unless @options.html_template
-      @html_composer.create_main(toc, body, h1)
+      composer = @options.html_template ? @html_composer : @plain_composer
+      composer.create_main(toc, body, h1)
     end
 
     def create_style(path_to_css_file)
