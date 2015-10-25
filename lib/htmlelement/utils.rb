ec567ad77de20cad955fb202e0ddaa8e6bb8694f
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-10-25 12:21:06 +0900

    implemented Utils::LinkManager#unify_host_names()

--- lib/htmlelement/utils.rb
+++ lib/htmlelement/utils.rb
@@ -6,15 +6,25 @@ require 'uri'
 class HtmlElement
   module Utils
     class LinkManager
-      def initialize(domain_name, dir_in_domain, from_names)
+      def initialize(domain_name, from_host_names)
+        @domain_name = URI.parse(domain_name)
+        @domain_name_re = Regexp.compile(Regexp.escape(domain_name))
+        @from_host_names_re = compile_from_names_re(from_host_names)
       end
 
-      def unify_domain_names(url)
-        false
+      def unify_host_names(url)
+        url.sub(@from_host_names_re, @domain_name.host)
       end
 
       def convert_in_relative(url)
         false
+      end
+
+      private
+
+      def compile_from_names_re(from_host_names)
+        escaped_names = from_host_names.map {|name| Regexp.escape(name) }
+        Regexp.compile(escaped_names.join("|"))
       end
     end
   end
--- test/test_htmlelement_utils.rb
+++ test/test_htmlelement_utils.rb
@@ -6,13 +6,15 @@ require 'lib/htmlelement/utils'
 
 class TC_HtmlElement < MiniTest::Unit::TestCase
   def setup
-    @link_manager = HtmlElement::Utils::LinkManager.new("http://www.example.org",
-                                                        "default_path",
+    @link_manager = HtmlElement::Utils::LinkManager.new("http://www.example.org/default_path",
                                                         ["stage.example.org", "develop.example.org"])
   end
 
-  def test_link_manager_unify_domain_names
-    assert(@link_manager.unify_domain_names("http://stage.example.org/path1"), "Not implemented yet")
+  def test_link_manager_unify_host_names
+    assert_equal("http://www.example.org/path1",
+                 @link_manager.unify_host_names("http://stage.example.org/path1"))
+    assert_equal("http://www.example.org/path1/path1-1",
+                 @link_manager.unify_host_names("http://develop.example.org/path1/path1-1"))
   end
 
   def test_link_manager_convert_in_relative
