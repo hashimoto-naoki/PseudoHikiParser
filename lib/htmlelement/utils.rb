1587d4a787f030347ca5bea9cdb8d61a7f6507d6
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-10-25 15:29:47 +0900

    update Utils::LinkManager#convert_to_relative_path(): it returns "./" when the default domain name is passed

--- lib/htmlelement/utils.rb
+++ lib/htmlelement/utils.rb
@@ -6,6 +6,8 @@ require 'uri'
 class HtmlElement
   module Utils
     class LinkManager
+      SEP = "/"
+
       def initialize(domain_name, from_host_names)
         domain_name = domain_name + "/" unless domain_name.end_with?("/")
         @domain_name = URI.parse(domain_name)
@@ -18,6 +20,7 @@ class HtmlElement
       end
 
       def convert_to_relative_path(url)
+        return "./".freeze if default_domain?(url)
         (URI.parse(url) - @domain_name).to_s
       end
 
@@ -26,6 +29,11 @@ class HtmlElement
       def compile_from_names_re(from_host_names)
         escaped_names = from_host_names.map {|name| Regexp.escape(name) }
         Regexp.compile(escaped_names.join("|"))
+      end
+
+      def default_domain?(url)
+        url = url + SEP unless url.end_with?(SEP)
+        (URI.parse(url) - @domain_name).to_s.empty?
       end
     end
   end
