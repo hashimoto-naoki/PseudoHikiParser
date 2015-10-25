5cf82c6e4a157564e68cdddfd64e043ffdd7d6e7
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-10-25 13:48:38 +0900

    test_htmlelement_utils.rb: renamed test methods

--- test/test_htmlelement_utils.rb
+++ test/test_htmlelement_utils.rb
@@ -4,20 +4,20 @@ require 'minitest/autorun'
 require 'lib/htmlelement'
 require 'lib/htmlelement/utils'
 
-class TC_HtmlElement < MiniTest::Unit::TestCase
+class TC_HtmlElement_Utils_LinkManager < MiniTest::Unit::TestCase
   def setup
     @link_manager = HtmlElement::Utils::LinkManager.new("http://www.example.org/default_path",
                                                         ["stage.example.org", "develop.example.org"])
   end
 
-  def test_link_manager_unify_host_names
+  def test_unify_host_names
     assert_equal("http://www.example.org/path1",
                  @link_manager.unify_host_names("http://stage.example.org/path1"))
     assert_equal("http://www.example.org/path1/path1-1",
                  @link_manager.unify_host_names("http://develop.example.org/path1/path1-1"))
   end
 
-  def test_link_manager_convert_to_relative_path
+  def test_convert_to_relative_path
     assert_equal("../path1", @link_manager.convert_to_relative_path("http://www.example.org/path1"))
     assert_equal("../path1/path1-1/",
                  @link_manager.convert_to_relative_path("http://www.example.org/path1/path1-1/"))
