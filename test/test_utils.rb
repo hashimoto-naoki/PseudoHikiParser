eda731ba52be056ccdcdbdb8b86b3c2684519d59
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2015-11-17 23:04:34 +0900

    added a test for PseudoHiki::Utils::TableManager#determine_header_scope()

--- test/test_utils.rb
+++ test/test_utils.rb
@@ -37,5 +37,27 @@ TEXT
 
     assert_equal(selected_headings, nodes)
   end
+
+  def test_table_manager_determine_header_scope
+    table_with_row_header_text = <<TABLE
+||!header1||!header2||!header3
+||row1-1||row1-2||row1-3
+||row2-1||row2-2||row2-3
+TABLE
+
+table_with_col_header_text = <<TABLE
+||!header1||col1-1||col2-1
+||!header2||col1-2||col2-2
+||!header3||col1-3||col2-3
+TABLE
+
+    table_manager = PseudoHiki::Utils::TableManager.new
+
+    table_with_row_header = PseudoHiki::BlockParser.parse(table_with_row_header_text)[0]
+    assert_equal(:col_scope, table_manager.determine_header_scope(table_with_row_header))
+
+    table_with_col_header = PseudoHiki::BlockParser.parse(table_with_col_header_text)[0]
+    assert_equal(:row_scope, table_manager.determine_header_scope(table_with_col_header))
+  end
 end
 
