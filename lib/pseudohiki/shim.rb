74329ed2c5917680c37f3c6f195a46c7510fac45
HASHIMOTO, Naoki <hashimoto.naoki@gmail.com>
2016-09-22 20:13:05 +0900

    move the definition of String#encode into shim.rb

--- lib/pseudohiki/shim.rb
+++ lib/pseudohiki/shim.rb
@@ -7,3 +7,24 @@ unless //.respond_to? :match?
     end
   end
 end
+
+unless String.new.respond_to? :encode
+  require 'iconv'
+
+  class String
+    def choose_input_encoding_using_kcode
+      PseudoHiki::OptionManager::ENCODING_REGEXP.each do |pat, encoding|
+        return PseudoHiki::OptionManager::ENCODING_TO_CHARSET[encoding] if pat.match? $KCODE
+      end
+      HtmlElement::CHARSET::UTF8
+    end
+    private :choose_input_encoding_using_kcode
+
+    def encode(to, from=choose_input_encoding_using_kcode)
+      iconv = Iconv.new(to, from)
+      str = iconv.iconv(self)
+      str << iconv.iconv(nil)
+    end
+    public :encode
+  end
+end
